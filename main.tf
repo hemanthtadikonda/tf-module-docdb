c57c931c84cb3f10e0ae7b20e311ccccac3f8978
hemanth <hemanthtadikonda29@gmail.com>
2023-08-24 15:16:40 +0530

    parameter

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "${var.env}-docdb-cluster"
   engine                  = "docdb"
   engine_version          = var.engine_version
-  master_username         = data.aws_ssm_parameter.master_username
-  master_password         = data.aws_ssm_parameter.master_password
+  master_username         = data.aws_ssm_parameter.master_username.value
+  master_password         = data.aws_ssm_parameter.master_password.value
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = var.skip_final_snapshot
